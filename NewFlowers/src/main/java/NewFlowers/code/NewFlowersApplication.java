9583c07b8375dc30eb14450ba2f29e26ed763514
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-12-10 19:37:43 +0000

    initail commit

---
+++ NewFlowers/src/main/java/NewFlowers/code/NewFlowersApplication.java
@@ -0,0 +1,12 @@
+package NewFlowers.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFlowersApplication.class, args);
+	}
+}
